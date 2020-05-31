6ff7875826e4b95982e5d6feb643ccb9c75b14bc
Arkadev Banerjee <arkadev220701@gmail.com>
2020-05-31 17:19:23 +0530

    Added Files

---
+++ javascript2.js
@@ -0,0 +1,53 @@
+$(window).on('scroll',function(){
+
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > 700) {
+        $('.top-menu').removeClass('activated');
+    } else {
+        $('.top-menu').addClass('activated');
+    }
+
+    if(stop>700 && stop<1400) {
+    	$('.next-menu-1').addClass('activated');
+    }
+    else{
+    	$('.next-menu-1').removeClass('activated');
+    }
+
+    if(stop>1400 && stop<2200) {
+    	$('.next-menu-2').addClass('activated');
+    }
+    else{
+    	$('.next-menu-2').removeClass('activated');
+    }
+
+    if(stop>2200 && stop<2800) {
+    	$('.next-menu-3').addClass('activated');
+    }
+    else{
+    	$('.next-menu-3').removeClass('activated');
+    }
+    
+    if(stop>2800 && stop<3600) {
+    	$('.next-menu-4').addClass('activated');
+    }
+    else{
+    	$('.next-menu-4').removeClass('activated');
+    }
+    
+    if(stop>3600 && stop<4900) {
+    	$('.next-menu-5').addClass('activated');
+    }
+    else{
+    	$('.next-menu-5').removeClass('activated');
+    }
+    
+    if(stop>4900) {
+    	$('.next-menu-6').addClass('activated');
+    }
+    else{
+    	$('.next-menu-6').removeClass('activated');
+    }
+    
+});
