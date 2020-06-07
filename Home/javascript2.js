2e1071d422b2b7f0210dd5258213e30a05d6e6c0
Arkadev Banerjee <arkadev220701@gmail.com>
2020-06-07 20:44:57 +0530

    Fixed Navbar Active Problem 2nd

--- Home/javascript2.js
+++ Home/javascript2.js
@@ -8,37 +8,37 @@ $(window).on('scroll', function() {
         $('.top-menu').addClass('activated');
     }
 
-    if (stop > 700 && stop <= 1400) {
+    if (stop > 700 && stop <= 1260) {
         $('.next-menu-1').addClass('activated');
     } else {
         $('.next-menu-1').removeClass('activated');
     }
 
-    if (stop > 1400 && stop <= 2100) {
+    if (stop > 1260 && stop <= 1960) {
         $('.next-menu-2').addClass('activated');
     } else {
         $('.next-menu-2').removeClass('activated');
     }
 
-    if (stop > 2100 && stop <= 2900) {
+    if (stop > 1960 && stop <= 2760) {
         $('.next-menu-3').addClass('activated');
     } else {
         $('.next-menu-3').removeClass('activated');
     }
 
-    if (stop > 2900 && stop <= 3650) {
+    if (stop > 2760 && stop <= 3510) {
         $('.next-menu-4').addClass('activated');
     } else {
         $('.next-menu-4').removeClass('activated');
     }
 
-    if (stop > 3650 && stop <= 5100) {
+    if (stop > 3510 && stop <= 4960) {
         $('.next-menu-5').addClass('activated');
     } else {
         $('.next-menu-5').removeClass('activated');
     }
 
-    if (stop > 5100) {
+    if (stop > 4960) {
         $('.next-menu-6').addClass('activated');
     } else {
         $('.next-menu-6').removeClass('activated');
